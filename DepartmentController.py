f7dfb227bdab92d35af7d24c7f0dd9a19e4ff1da
Siwat <siwat.keng.pongpanit@gmail.com>
2019-11-20 15:01:16 +0700

    connect add appoint with controller

--- DepartmentController.py
+++ DepartmentController.py
@@ -8,30 +8,35 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+import appointmentAdd
 
 
 class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 300)
-        self.pushButton_6 = QtWidgets.QPushButton(Dialog)
+        
+    def __init__(self):
+        self.Dialog = QtWidgets.QDialog()
+        self.Dialog.setObjectName("Dialog")
+        self.Dialog.resize(400, 300)
+        self.pushButton_6 = QtWidgets.QPushButton(self.Dialog)
         self.pushButton_6.setGeometry(QtCore.QRect(150, 250, 93, 28))
         self.pushButton_6.setObjectName("pushButton_6")
-        self.pushButton_3 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_3 = QtWidgets.QPushButton(self.Dialog)
         self.pushButton_3.setGeometry(QtCore.QRect(230, 30, 141, 28))
         self.pushButton_3.setObjectName("pushButton_3")
-        self.pushButton_4 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_4 = QtWidgets.QPushButton(self.Dialog)
         self.pushButton_4.setGeometry(QtCore.QRect(230, 100, 141, 28))
         self.pushButton_4.setObjectName("pushButton_4")
-        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton = QtWidgets.QPushButton(self.Dialog)
         self.pushButton.setGeometry(QtCore.QRect(30, 30, 141, 28))
         self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2 = QtWidgets.QPushButton(self.Dialog)
         self.pushButton_2.setGeometry(QtCore.QRect(30, 100, 141, 28))
         self.pushButton_2.setObjectName("pushButton_2")
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.pushButton.clicked.connect(self.open_appointment_add)
+
+        self.retranslateUi(self.Dialog)
+        QtCore.QMetaObject.connectSlotsByName(self.Dialog)
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
@@ -42,12 +47,18 @@ class Ui_Dialog(object):
         self.pushButton.setText(_translate("Dialog", "Add New Department"))
         self.pushButton_2.setText(_translate("Dialog", "Edit Department"))
 
+    def open_appointment_add(self):
+        self.ui = appointmentAdd.Ui_Dialog()
+        self.Dialog.hide()
+        self.ui.show()
+
+    def show(self):
+        self.Dialog.show()
+
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
     ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    ui.show()
     sys.exit(app.exec_())
--- appointmentAdd.py
+++ appointmentAdd.py
@@ -8,53 +8,59 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+import DepartmentController
 
 
 class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(414, 394)
-        self.pushButton = QtWidgets.QPushButton(Dialog)
+
+        
+    def __init__(self):
+        self.Dialog = QtWidgets.QDialog()
+        self.Dialog.setObjectName("Dialog")
+        self.Dialog.resize(414, 394)
+        self.pushButton = QtWidgets.QPushButton(self.Dialog)
         self.pushButton.setGeometry(QtCore.QRect(100, 330, 121, 28))
         self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton.clicked.connect(self.on_pushButton_clicked)
+        self.pushButton_2 = QtWidgets.QPushButton(self.Dialog)
         self.pushButton_2.setGeometry(QtCore.QRect(250, 330, 93, 28))
         self.pushButton_2.setObjectName("pushButton_2")
-        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+        self.pushButton_2.clicked.connect(self.on_pushButton_2_clicked)
+        self.lineEdit = QtWidgets.QLineEdit(self.Dialog)
         self.lineEdit.setGeometry(QtCore.QRect(140, 60, 171, 22))
         self.lineEdit.setObjectName("lineEdit")
-        self.label = QtWidgets.QLabel(Dialog)
+        self.label = QtWidgets.QLabel(self.Dialog)
         self.label.setGeometry(QtCore.QRect(30, 60, 101, 20))
         self.label.setObjectName("label")
-        self.dateEdit = QtWidgets.QDateEdit(Dialog)
+        self.dateEdit = QtWidgets.QDateEdit(self.Dialog)
         self.dateEdit.setGeometry(QtCore.QRect(170, 110, 110, 22))
         self.dateEdit.setCalendarPopup(True)
         self.dateEdit.setObjectName("dateEdit")
-        self.timeEdit = QtWidgets.QTimeEdit(Dialog)
+        self.timeEdit = QtWidgets.QTimeEdit(self.Dialog)
         self.timeEdit.setGeometry(QtCore.QRect(170, 160, 118, 22))
         self.timeEdit.setCalendarPopup(False)
         self.timeEdit.setObjectName("timeEdit")
-        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2 = QtWidgets.QLabel(self.Dialog)
         self.label_2.setGeometry(QtCore.QRect(40, 110, 111, 20))
         self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3 = QtWidgets.QLabel(self.Dialog)
         self.label_3.setGeometry(QtCore.QRect(40, 160, 121, 20))
         self.label_3.setObjectName("label_3")
-        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.Dialog)
         self.lineEdit_2.setGeometry(QtCore.QRect(140, 20, 171, 22))
         self.lineEdit_2.setObjectName("lineEdit_2")
-        self.label_4 = QtWidgets.QLabel(Dialog)
+        self.label_4 = QtWidgets.QLabel(self.Dialog)
         self.label_4.setGeometry(QtCore.QRect(60, 20, 71, 20))
         self.label_4.setObjectName("label_4")
-        self.plainTextEdit = QtWidgets.QPlainTextEdit(Dialog)
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.Dialog)
         self.plainTextEdit.setGeometry(QtCore.QRect(170, 200, 171, 87))
         self.plainTextEdit.setObjectName("plainTextEdit")
-        self.label_5 = QtWidgets.QLabel(Dialog)
+        self.label_5 = QtWidgets.QLabel(self.Dialog)
         self.label_5.setGeometry(QtCore.QRect(10, 200, 151, 20))
         self.label_5.setObjectName("label_5")
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(self.Dialog)
+        QtCore.QMetaObject.connectSlotsByName(self.Dialog)
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
@@ -66,13 +72,40 @@ class Ui_Dialog(object):
         self.label_3.setText(_translate("Dialog", "Appointment Time :"))
         self.label_4.setText(_translate("Dialog", " Patient ID :"))
         self.label_5.setText(_translate("Dialog", "Appointment Description :"))
+ 
+        
+
+    def on_pushButton_clicked(self):
+        patientID = self.lineEdit_2.text()
+        appointID = self.lineEdit.text()
+        date = self.dateEdit.date()
+        time = self.timeEdit.time()
+        desc = self.plainTextEdit.toPlainText()
+
+        #---------add data to database--------
+        print(patientID)
+        print(appointID)
+        print(date.day(),date.month(),date.year())
+        print(time.hour(),time.minute())
+        print(desc)
+        #-------------------------------------
+        try:
+            self.ui = DepartmentController.Ui_Dialog()
+            self.ui.show()
+            self.Dialog.close()
+        except Exception as e:
+            print(e)
+        
+    def on_pushButton_2_clicked(self): # Testing
+        print('cancel')
+
+    def show(self):
+        self.Dialog.show()
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
     ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    ui.show()
     sys.exit(app.exec_())
