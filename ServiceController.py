967bfeea9890e02128d1de9e2f61cfe13e54bc64
Siwat <siwat.keng.pongpanit@gmail.com>
2019-11-20 21:58:02 +0700

    connecting service

--- ServiceController.py
+++ ServiceController.py
@@ -8,30 +8,38 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
+import MainController
+import serviceAdd,serviceDelete,serviceEdit,serviceSearch
 
 class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 300)
-        self.pushButton_4 = QtWidgets.QPushButton(Dialog)
+    def __init__(self):
+        self.Dialog = QtWidgets.QDialog()
+        self.Dialog.setObjectName("Dialog")
+        self.Dialog.resize(400, 300)
+        self.pushButton_4 = QtWidgets.QPushButton(self.Dialog)
         self.pushButton_4.setGeometry(QtCore.QRect(230, 100, 141, 28))
         self.pushButton_4.setObjectName("pushButton_4")
-        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton = QtWidgets.QPushButton(self.Dialog)
         self.pushButton.setGeometry(QtCore.QRect(30, 30, 141, 28))
         self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2 = QtWidgets.QPushButton(self.Dialog)
         self.pushButton_2.setGeometry(QtCore.QRect(30, 100, 141, 28))
         self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_6 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_6 = QtWidgets.QPushButton(self.Dialog)
         self.pushButton_6.setGeometry(QtCore.QRect(150, 250, 93, 28))
         self.pushButton_6.setObjectName("pushButton_6")
-        self.pushButton_3 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_3 = QtWidgets.QPushButton(self.Dialog)
         self.pushButton_3.setGeometry(QtCore.QRect(230, 30, 141, 28))
         self.pushButton_3.setObjectName("pushButton_3")
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(self.Dialog)
+        QtCore.QMetaObject.connectSlotsByName(self.Dialog)
+
+        self.pushButton.clicked.connect(self.add_service)
+        self.pushButton_2.clicked.connect(self.edit_service)
+        self.pushButton_3.clicked.connect(self.delete_service)
+        self.pushButton_4.clicked.connect(self.search_service)
+        self.pushButton_6.clicked.connect(self.back)        
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
@@ -43,11 +51,38 @@ class Ui_Dialog(object):
         self.pushButton_3.setText(_translate("Dialog", "Delete Service"))
 
 
+    def show(self):
+        self.Dialog.show()
+
+    def back(self):
+        self.ui = MainController.Ui_Dialog()
+        self.ui.show()
+        self.Dialog.close()
+
+    def add_service(self):
+        self.ui = serviceAdd.Ui_Dialog()
+        self.ui.show()
+        self.Dialog.close()
+        
+    def edit_service(self):
+        self.ui = serviceEdit.Ui_Dialog()
+        self.ui.show()
+        self.Dialog.close()
+
+    def delete_service(self):
+        self.ui = serviceDelete.Ui_Dialog()
+        self.ui.show()
+        self.Dialog.close()
+
+    def search_service(self):
+        self.ui = serviceSearch.Ui_Dialog()
+        self.ui.show()
+        self.Dialog.close()        
+
+        
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
     ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    ui.show()
     sys.exit(app.exec_())
--- serviceAdd.py
+++ serviceAdd.py
@@ -8,39 +8,40 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
+import ServiceController
 
 class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 300)
-        self.pushButton = QtWidgets.QPushButton(Dialog)
+    def __init__(self):
+        self.Dialog = QtWidgets.QDialog()
+        self.Dialog.setObjectName("Dialog")
+        self.Dialog.resize(400, 300)
+        self.pushButton = QtWidgets.QPushButton(self.Dialog)
         self.pushButton.setGeometry(QtCore.QRect(90, 190, 121, 28))
         self.pushButton.setObjectName("pushButton")
-        self.label_4 = QtWidgets.QLabel(Dialog)
+        self.label_4 = QtWidgets.QLabel(self.Dialog)
         self.label_4.setGeometry(QtCore.QRect(100, 30, 101, 20))
         self.label_4.setObjectName("label_4")
-        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.Dialog)
         self.lineEdit_2.setGeometry(QtCore.QRect(180, 30, 171, 22))
         self.lineEdit_2.setObjectName("lineEdit_2")
-        self.comboBox = QtWidgets.QComboBox(Dialog)
+        self.comboBox = QtWidgets.QComboBox(self.Dialog)
         self.comboBox.setGeometry(QtCore.QRect(190, 120, 151, 22))
         self.comboBox.setObjectName("comboBox")
-        self.label_7 = QtWidgets.QLabel(Dialog)
+        self.label_7 = QtWidgets.QLabel(self.Dialog)
         self.label_7.setGeometry(QtCore.QRect(90, 120, 101, 20))
         self.label_7.setObjectName("label_7")
-        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2 = QtWidgets.QPushButton(self.Dialog)
         self.pushButton_2.setGeometry(QtCore.QRect(240, 190, 93, 28))
         self.pushButton_2.setObjectName("pushButton_2")
-        self.lineEdit_3 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.Dialog)
         self.lineEdit_3.setGeometry(QtCore.QRect(180, 70, 171, 22))
         self.lineEdit_3.setObjectName("lineEdit_3")
-        self.label_5 = QtWidgets.QLabel(Dialog)
+        self.label_5 = QtWidgets.QLabel(self.Dialog)
         self.label_5.setGeometry(QtCore.QRect(80, 70, 121, 20))
         self.label_5.setObjectName("label_5")
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(self.Dialog)
+        QtCore.QMetaObject.connectSlotsByName(self.Dialog)
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
@@ -52,11 +53,13 @@ class Ui_Dialog(object):
         self.label_5.setText(_translate("Dialog", " Service Name :"))
 
 
+    def show(self):
+        self.Dialog.show()
+
+        
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
     ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    ui.show()
     sys.exit(app.exec_())
