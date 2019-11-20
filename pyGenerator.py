eb05b15bf0ac4f46e00aa10c1123026695b3624a
เศรษฐ์ ภูภักดิ์ <Seth214@hotmail.com>
2019-11-20 10:31:44 +0700

    change generator

--- pyGenerator.py
+++ pyGenerator.py
@@ -4,10 +4,10 @@ from os.path import isfile,join
 
 def main():
     mypath = os.getcwd()
-    onlyfile = [f for f in listdir(mypath) if isfile(join(mypath, f)) and str(f).endswith('.ui')]
+    onlyfile = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f.endswith('.ui') and f.replace('.ui','.py') not in listdir(mypath)]
 
     for i in onlyfile:
-        os.system('pyuic5 -x '+str(i)+' -o '+str(i).replace('.ui','.py'))
+        os.system('pyuic5 -x '+i+' -o '+i.replace('.ui','.py'))
 
 if __name__ == "__main__":
     main()
