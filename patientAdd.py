e500e9c84c93e36e0236cb4eb5fd639e2c352d98
Siwat <siwat.keng.pongpanit@gmail.com>
2019-11-25 14:21:08 +0700

    changed patient add/delete

--- patientAdd.py
+++ patientAdd.py
@@ -1,178 +1,209 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-import patientController,inpatientPopup
+import patientController
 
 class Ui_Dialog(object):
     def __init__(self):
         self.Dialog = QtWidgets.QDialog()
         self.Dialog.setObjectName("Dialog")
-        self.Dialog.resize(502, 464)
+        self.Dialog.resize(443, 637)
         self.label = QtWidgets.QLabel(self.Dialog)
         self.label.setGeometry(QtCore.QRect(80, 30, 71, 20))
         self.label.setObjectName("label")
-        self.lineEdit = QtWidgets.QLineEdit(self.Dialog)
-        self.lineEdit.setGeometry(QtCore.QRect(150, 30, 171, 22))
-        self.lineEdit.setObjectName("lineEdit")
+        self.pid = QtWidgets.QLineEdit(self.Dialog)
+        self.pid.setGeometry(QtCore.QRect(150, 30, 171, 22))
+        self.pid.setObjectName("pid")
         self.label_2 = QtWidgets.QLabel(self.Dialog)
-        self.label_2.setGeometry(QtCore.QRect(100, 70, 41, 20))
+        self.label_2.setGeometry(QtCore.QRect(60, 70, 91, 20))
         self.label_2.setObjectName("label_2")
-        self.lineEdit_2 = QtWidgets.QLineEdit(self.Dialog)
-        self.lineEdit_2.setGeometry(QtCore.QRect(150, 70, 171, 22))
-        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.name = QtWidgets.QLineEdit(self.Dialog)
+        self.name.setGeometry(QtCore.QRect(150, 70, 171, 22))
+        self.name.setObjectName("name")
         self.label_3 = QtWidgets.QLabel(self.Dialog)
         self.label_3.setGeometry(QtCore.QRect(70, 110, 81, 20))
         self.label_3.setObjectName("label_3")
-        self.lineEdit_3 = QtWidgets.QLineEdit(self.Dialog)
-        self.lineEdit_3.setGeometry(QtCore.QRect(150, 110, 171, 22))
-        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.nid = QtWidgets.QLineEdit(self.Dialog)
+        self.nid.setGeometry(QtCore.QRect(150, 110, 171, 22))
+        self.nid.setObjectName("nid")
         self.label_4 = QtWidgets.QLabel(self.Dialog)
         self.label_4.setGeometry(QtCore.QRect(90, 160, 51, 20))
         self.label_4.setObjectName("label_4")
-
-        self.gender_group=QtWidgets.QButtonGroup(self.Dialog)
-        
-        self.radioButton = QtWidgets.QRadioButton(self.Dialog)
-        self.radioButton.setGeometry(QtCore.QRect(160, 160, 95, 20))
-        self.radioButton.setObjectName("radioButton")
-        self.radioButton_2 = QtWidgets.QRadioButton(self.Dialog)
-        self.radioButton_2.setGeometry(QtCore.QRect(270, 160, 95, 20))
-        self.radioButton_2.setObjectName("radioButton_2")
-
-        self.gender_group.addButton(self.radioButton)
-        self.gender_group.addButton(self.radioButton_2)
-
-        
+        self.male = QtWidgets.QRadioButton(self.Dialog)
+        self.male.setGeometry(QtCore.QRect(160, 160, 95, 20))
+        self.male.setObjectName("male")
+        self.buttonGroup = QtWidgets.QButtonGroup(self.Dialog)
+        self.buttonGroup.setObjectName("buttonGroup")
+        self.buttonGroup.addButton(self.male)
+        self.female = QtWidgets.QRadioButton(self.Dialog)
+        self.female.setGeometry(QtCore.QRect(270, 160, 95, 20))
+        self.female.setObjectName("female")
+        self.buttonGroup.addButton(self.female)
         self.label_5 = QtWidgets.QLabel(self.Dialog)
         self.label_5.setGeometry(QtCore.QRect(70, 200, 71, 20))
         self.label_5.setObjectName("label_5")
-        self.dateEdit = QtWidgets.QDateEdit(self.Dialog)
-        self.dateEdit.setGeometry(QtCore.QRect(170, 200, 110, 22))
-        self.dateEdit.setCurrentSection(QtWidgets.QDateTimeEdit.DaySection)
-        self.dateEdit.setCalendarPopup(True)
-        self.dateEdit.setTimeSpec(QtCore.Qt.LocalTime)
-        self.dateEdit.setDate(QtCore.QDate(2000, 1, 2))
-        self.dateEdit.setObjectName("dateEdit")
+        self.DoB = QtWidgets.QDateEdit(self.Dialog)
+        self.DoB.setGeometry(QtCore.QRect(170, 200, 110, 22))
+        self.DoB.setCurrentSection(QtWidgets.QDateTimeEdit.DaySection)
+        self.DoB.setCalendarPopup(True)
+        self.DoB.setTimeSpec(QtCore.Qt.LocalTime)
+        self.DoB.setDate(QtCore.QDate(2000, 1, 2))
+        self.DoB.setObjectName("DoB")
         self.label_6 = QtWidgets.QLabel(self.Dialog)
         self.label_6.setGeometry(QtCore.QRect(60, 240, 81, 20))
         self.label_6.setObjectName("label_6")
-
-        self.blood_group=QtWidgets.QButtonGroup(self.Dialog)
-        
-        self.radioButton_3 = QtWidgets.QRadioButton(self.Dialog)
-        self.radioButton_3.setGeometry(QtCore.QRect(160, 240, 51, 20))
-        self.radioButton_3.setObjectName("radioButton_3")
-        self.radioButton_4 = QtWidgets.QRadioButton(self.Dialog)
-        self.radioButton_4.setGeometry(QtCore.QRect(210, 240, 41, 20))
-        self.radioButton_4.setObjectName("radioButton_4")
-        self.radioButton_5 = QtWidgets.QRadioButton(self.Dialog)
-        self.radioButton_5.setGeometry(QtCore.QRect(260, 240, 41, 20))
-        self.radioButton_5.setObjectName("radioButton_5")
-        self.radioButton_6 = QtWidgets.QRadioButton(self.Dialog)
-        self.radioButton_6.setGeometry(QtCore.QRect(300, 240, 41, 20))
-        self.radioButton_6.setObjectName("radioButton_6")
-
-        self.blood_group.addButton(self.radioButton_3)
-        self.blood_group.addButton(self.radioButton_4)
-        self.blood_group.addButton(self.radioButton_5)
-        self.blood_group.addButton(self.radioButton_6)
-        
-        self.pushButton = QtWidgets.QPushButton(self.Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(80, 400, 93, 28))
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(self.Dialog)
-        self.pushButton_2.setGeometry(QtCore.QRect(230, 400, 93, 28))
-        self.pushButton_2.setObjectName("pushButton_2")
+        self.bloodO = QtWidgets.QRadioButton(self.Dialog)
+        self.bloodO.setGeometry(QtCore.QRect(160, 240, 51, 20))
+        self.bloodO.setObjectName("bloodO")
+        self.buttonGroup_2 = QtWidgets.QButtonGroup(self.Dialog)
+        self.buttonGroup_2.setObjectName("buttonGroup_2")
+        self.buttonGroup_2.addButton(self.bloodO)
+        self.bloodA = QtWidgets.QRadioButton(self.Dialog)
+        self.bloodA.setGeometry(QtCore.QRect(210, 240, 41, 20))
+        self.bloodA.setObjectName("bloodA")
+        self.buttonGroup_2.addButton(self.bloodA)
+        self.bloodB = QtWidgets.QRadioButton(self.Dialog)
+        self.bloodB.setGeometry(QtCore.QRect(260, 240, 41, 20))
+        self.bloodB.setObjectName("bloodB")
+        self.buttonGroup_2.addButton(self.bloodB)
+        self.bloodAB = QtWidgets.QRadioButton(self.Dialog)
+        self.bloodAB.setGeometry(QtCore.QRect(300, 240, 41, 20))
+        self.bloodAB.setObjectName("bloodAB")
+        self.buttonGroup_2.addButton(self.bloodAB)
+        self.ok = QtWidgets.QPushButton(self.Dialog)
+        self.ok.setGeometry(QtCore.QRect(80, 580, 93, 28))
+        self.ok.setObjectName("ok")
+        self.cancel = QtWidgets.QPushButton(self.Dialog)
+        self.cancel.setGeometry(QtCore.QRect(230, 580, 93, 28))
+        self.cancel.setObjectName("cancel")
         self.label_7 = QtWidgets.QLabel(self.Dialog)
-        self.label_7.setGeometry(QtCore.QRect(90, 320, 51, 20))
+        self.label_7.setGeometry(QtCore.QRect(90, 460, 51, 20))
         self.label_7.setObjectName("label_7")
-        self.lineEdit_4 = QtWidgets.QLineEdit(self.Dialog)
-        self.lineEdit_4.setGeometry(QtCore.QRect(150, 320, 171, 22))
-        self.lineEdit_4.setObjectName("lineEdit_4")
-        self.label_8 = QtWidgets.QLabel(self.Dialog)
-        self.label_8.setGeometry(QtCore.QRect(80, 280, 61, 20))
+        self.phone = QtWidgets.QLineEdit(self.Dialog)
+        self.phone.setGeometry(QtCore.QRect(150, 460, 171, 22))
+        self.phone.setObjectName("phone")
+        self.label_8 = QtWidgets.QLabel(Dialog)
+        self.label_8.setGeometry(QtCore.QRect(80, 420, 61, 20))
         self.label_8.setObjectName("label_8")
-        self.lineEdit_5 = QtWidgets.QLineEdit(self.Dialog)
-        self.lineEdit_5.setGeometry(QtCore.QRect(150, 280, 171, 22))
-        self.lineEdit_5.setObjectName("lineEdit_5")
+        self.allergic = QtWidgets.QLineEdit(self.Dialog)
+        self.allergic.setGeometry(QtCore.QRect(150, 420, 171, 22))
+        self.allergic.setObjectName("allergic")
+        self.line = QtWidgets.QFrame(self.Dialog)
+        self.line.setGeometry(QtCore.QRect(57, 400, 351, 20))
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.label_9 = QtWidgets.QLabel(self.Dialog)
+        self.label_9.setGeometry(QtCore.QRect(194, 390, 61, 20))
+        self.label_9.setObjectName("label_9")
+        self.label_10 = QtWidgets.QLabel(self.Dialog)
+        self.label_10.setGeometry(QtCore.QRect(90, 280, 51, 20))
+        self.label_10.setObjectName("label_10")
+        self.unknownStatus = QtWidgets.QRadioButton(self.Dialog)
+        self.unknownStatus.setGeometry(QtCore.QRect(160, 280, 81, 20))
+        self.unknownStatus.setObjectName("unknownStatus")
+        self.buttonGroup_3 = QtWidgets.QButtonGroup(self.Dialog)
+        self.buttonGroup_3.setObjectName("buttonGroup_3")
+        self.buttonGroup_3.addButton(self.unknownStatus)
+        self.dischargedStatus = QtWidgets.QRadioButton(self.Dialog)
+        self.dischargedStatus.setGeometry(QtCore.QRect(270, 280, 95, 20))
+        self.dischargedStatus.setObjectName("dischargedStatus")
+        self.buttonGroup_3.addButton(self.dischargedStatus)
+        self.admittedStatus = QtWidgets.QRadioButton(self.Dialog)
+        self.admittedStatus.setGeometry(QtCore.QRect(160, 330, 81, 20))
+        self.admittedStatus.setObjectName("admittedStatus")
+        self.buttonGroup_3.addButton(self.admittedStatus)
+        self.deceasedStatus = QtWidgets.QRadioButton(self.Dialog)
+        self.deceasedStatus.setGeometry(QtCore.QRect(270, 330, 95, 20))
+        self.deceasedStatus.setObjectName("deceasedStatus")
+        self.buttonGroup_3.addButton(self.deceasedStatus)
+        self.medhistory = QtWidgets.QLineEdit(self.Dialog)
+        self.medhistory.setGeometry(QtCore.QRect(150, 500, 171, 22))
+        self.medhistory.setObjectName("medhistory")
+        self.label_11 = QtWidgets.QLabel(self.Dialog)
+        self.label_11.setGeometry(QtCore.QRect(40, 500, 101, 20))
+        self.label_11.setObjectName("label_11")
 
         self.retranslateUi(self.Dialog)
         QtCore.QMetaObject.connectSlotsByName(self.Dialog)
-
-        self.pushButton.clicked.connect(self.add)
-        self.pushButton_2.clicked.connect(self.back)
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
         self.label.setText(_translate("Dialog", "Patient ID :"))
-        self.label_2.setText(_translate("Dialog", "Name :"))
-        self.label_3.setText(_translate("Dialog", "Personal ID :"))
+        self.label_2.setText(_translate("Dialog", "Patient Name :"))
+        self.label_3.setText(_translate("Dialog", "National ID :"))
         self.label_4.setText(_translate("Dialog", "Gender :"))
-        self.radioButton.setText(_translate("Dialog", "Male"))
-        self.radioButton_2.setText(_translate("Dialog", "Female"))
+        self.male.setText(_translate("Dialog", "Male"))
+        self.female.setText(_translate("Dialog", "Female"))
         self.label_5.setText(_translate("Dialog", "Birth Date :"))
         self.label_6.setText(_translate("Dialog", "Blood Group :"))
-        self.radioButton_3.setText(_translate("Dialog", "O"))
-        self.radioButton_4.setText(_translate("Dialog", "A"))
-        self.radioButton_5.setText(_translate("Dialog", "B"))
-        self.radioButton_6.setText(_translate("Dialog", "AB"))
-        self.pushButton.setText(_translate("Dialog", "Ok"))
-        self.pushButton_2.setText(_translate("Dialog", "Cancel"))
+        self.bloodO.setText(_translate("Dialog", "O"))
+        self.bloodA.setText(_translate("Dialog", "A"))
+        self.bloodB.setText(_translate("Dialog", "B"))
+        self.bloodAB.setText(_translate("Dialog", "AB"))
+        self.ok.setText(_translate("Dialog", "Ok"))
+        self.cancel.setText(_translate("Dialog", "Cancel"))
         self.label_7.setText(_translate("Dialog", "Phone :"))
         self.label_8.setText(_translate("Dialog", "Allergic :"))
+        self.label_9.setText(_translate("Dialog", "Multivalue"))
+        self.label_10.setText(_translate("Dialog", "Status :"))
+        self.unknownStatus.setText(_translate("Dialog", "Unknown"))
+        self.dischargedStatus.setText(_translate("Dialog", "Discharged"))
+        self.admittedStatus.setText(_translate("Dialog", "Admitted"))
+        self.deceasedStatus.setText(_translate("Dialog", "Deceased"))
+        self.label_11.setText(_translate("Dialog", "Medical History :"))
+
 
     def show(self):
         self.Dialog.show()
 
+
     def add(self):
         gender = ""
         blood = ""
-        if self.radioButton.isChecked():
+        status = ""
+        if self.male.isChecked():
             gender = "Male"
-        elif self.radioButton_2.isChecked():
+        elif self.female.isChecked():
             gender = "Female"
-        if self.radioButton_3.isChecked():
+        if self.bloodO.isChecked():
             blood = "O"
-        elif self.radioButton_4.isChecked():
+        elif self.bloodA.isChecked():
             blood = "A"
-        elif self.radioButton_5.isChecked():
+        elif self.bloodB.isChecked():
             blood = "B"
-        elif self.radioButton_6.isChecked():
+        elif self.bloodAB.isChecked():
             blood = "AB"
-        patientID = self.lineEdit.text()
-        name = self.lineEdit_2.text()
-        personalID = self.lineEdit_3.text()
-        birthDate = self.dateEdit.date()
-        allergic = self.lineEdit_5.text()
-        phone =  self.lineEdit_4.text()
-        if patientID.upper().startswith("I"):
-            self.ui = inpatientPopup.Ui_Dialog(patientID,
-                                               name,
-                                               personalID,
-                                               birthDate,
-                                               allergic,
-                                               phone,
-                                               gender, 
-                                               blood)
-            self.Dialog.hide()
-            self.ui.show()
-        else:        
-
-            #TODO add new patient to database
+        if self.unknownStatus.isChecked():
+            status = "Unknown"
+        elif self.dischargedStatus.isChecked():
+            status = "Discharged"
+        elif self.admittedStatus.isChecked():
+            status = "Admitted"
+        elif self.deceasedStatus.isChecked():
+            status = "Deceased"
+        patientID = self.pid.text()
+        name = self.name.text()
+        personalID = self.nid.text()
+        birthDate = self.DoB.date()
+        allergic = self.allergic.text()
+        phone =  self.phone.text()
+        medHistory = self.medhistory.text()
+        #TODO add new patient to database
 
             
-            #get date by...
-            print(birthDate.day())
-            print(birthDate.month())
-            print(birthDate.year())
+        #get date by...
+        print(birthDate.day())
+        print(birthDate.month())
+        print(birthDate.year())
         
-            self.ui = patientController.Ui_Dialog()
-            self.ui.show()
-            self.Dialog.close()
+        self.ui = patientController.Ui_Dialog()
+        self.ui.show()
+        self.Dialog.close()
         
     def back(self):
         self.ui = patientController.Ui_Dialog()
         self.ui.show()
-        self.Dialog.close()
+        self.Dialog.close()        
         
 if __name__ == "__main__":
     import sys
--- patientDelete.py
+++ patientDelete.py
@@ -5,51 +5,55 @@ class Ui_Dialog(object):
     def __init__(self):
         self.Dialog = QtWidgets.QDialog()
         self.Dialog.setObjectName("Dialog")
-        self.Dialog.resize(442, 300)
+        self.Dialog.resize(442, 513)
         self.label = QtWidgets.QLabel(self.Dialog)
-        self.label.setGeometry(QtCore.QRect(20, 40, 121, 20))
+        self.label.setGeometry(QtCore.QRect(20, 50, 121, 20))
         self.label.setObjectName("label")
-        self.pushButton = QtWidgets.QPushButton(self.Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(100, 220, 93, 28))
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(self.Dialog)
-        self.pushButton_2.setGeometry(QtCore.QRect(250, 220, 93, 28))
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.lineEdit = QtWidgets.QLineEdit(self.Dialog)
-        self.lineEdit.setGeometry(QtCore.QRect(170, 40, 161, 22))
-        self.lineEdit.setObjectName("lineEdit")
+        self.delete_2 = QtWidgets.QPushButton(self.Dialog)
+        self.delete_2.setGeometry(QtCore.QRect(100, 460, 93, 28))
+        self.delete_2.setObjectName("delete_2")
+        self.cancel = QtWidgets.QPushButton(self.Dialog)
+        self.cancel.setGeometry(QtCore.QRect(250, 460, 93, 28))
+        self.cancel.setObjectName("cancel")
+        self.pid = QtWidgets.QLineEdit(self.Dialog)
+        self.pid.setGeometry(QtCore.QRect(170, 50, 161, 22))
+        self.pid.setObjectName("pid")
         self.textBrowser = QtWidgets.QTextBrowser(self.Dialog)
-        self.textBrowser.setGeometry(QtCore.QRect(140, 110, 161, 41))
+        self.textBrowser.setGeometry(QtCore.QRect(136, 150, 251, 261))
         self.textBrowser.setObjectName("textBrowser")
         self.label_2 = QtWidgets.QLabel(self.Dialog)
-        self.label_2.setGeometry(QtCore.QRect(34, 120, 101, 20))
+        self.label_2.setGeometry(QtCore.QRect(30, 160, 101, 20))
         self.label_2.setObjectName("label_2")
-        self.lineEdit_2 = QtWidgets.QLineEdit(self.Dialog)
-        self.lineEdit_2.setGeometry(QtCore.QRect(170, 70, 161, 22))
-        self.lineEdit_2.setObjectName("lineEdit_2")
-        self.label_3 = QtWidgets.QLabel(self.Dialog)
-        self.label_3.setGeometry(QtCore.QRect(20, 70, 141, 20))
-        self.label_3.setObjectName("label_3")
+        self.line = QtWidgets.QFrame(self.Dialog)
+        self.line.setGeometry(QtCore.QRect(50, 120, 351, 20))
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.fill = QtWidgets.QPushButton(self.Dialog)
+        self.fill.setGeometry(QtCore.QRect(340, 50, 93, 28))
+        self.fill.setObjectName("fill")
 
         self.retranslateUi(self.Dialog)
         QtCore.QMetaObject.connectSlotsByName(self.Dialog)
 
-        self.pushButton.clicked.connect(self.delete)
-        self.pushButton_2.clicked.connect(self.back)
+        self.delete_2.clicked.connect(self.delete)
+        self.cancel.clicked.connect(self.back)
+        self.fill.clicked.connect(self.fill)        
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
         self.label.setText(_translate("Dialog", "Delete by Patient ID :"))
-        self.pushButton.setText(_translate("Dialog", "Delete"))
-        self.pushButton_2.setText(_translate("Dialog", "Cancel"))
+        self.delete_2.setText(_translate("Dialog", "Delete"))
+        self.cancel.setText(_translate("Dialog", "Cancel"))
         self.textBrowser.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
         self.label_2.setText(_translate("Dialog", "Selected Patient :"))
-        self.label_3.setText(_translate("Dialog", "Delete by Patient Name :"))
+        self.fill.setText(_translate("Dialog", "Fill"))
+
 
     def show(self):
         self.Dialog.show()
@@ -60,15 +64,22 @@ class Ui_Dialog(object):
         self.ui.show()
 
     def delete(self):
-        patientID = self.lineEdit.text()
-        patientName = self.lineEdit_2.text()
-        #TODO add patient to self.textBrower then delete??? not sure haha
+        patientID = self.pid.text()
+        #TODO delete patient
         
+
+        self.ui = patientController.Ui_Dialog()
+        self.Dialog.close()
+        self.ui.show()
+
+    def fill(self):
         #example
         self.textBrowser.append('patient1')
         self.textBrowser.append('patient2')
         self.textBrowser.append('patient3')
 
+        
+        
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
