46f28f4fcd0208ea08e9130e6b143fd2650b5b75
Siwat <siwat.keng.pongpanit@gmail.com>
2019-11-25 18:02:40 +0700

    changed all drug

--- drugAdd.py
+++ drugAdd.py
@@ -1,48 +1,42 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
 import DrugController
+from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class Ui_Dialog(object):
     def __init__(self):
         self.Dialog = QtWidgets.QDialog()
         self.Dialog.setObjectName("Dialog")
-        self.Dialog.resize(400, 364)
-        self.textEdit = QtWidgets.QTextEdit(self.Dialog)
-        self.textEdit.setGeometry(QtCore.QRect(170, 140, 161, 87))
-        self.textEdit.setObjectName("textEdit")
-        self.pushButton_2 = QtWidgets.QPushButton(self.Dialog)
-        self.pushButton_2.setGeometry(QtCore.QRect(220, 270, 93, 28))
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.label_6 = QtWidgets.QLabel(self.Dialog)
-        self.label_6.setGeometry(QtCore.QRect(40, 140, 131, 20))
-        self.label_6.setObjectName("label_6")
-        self.pushButton = QtWidgets.QPushButton(self.Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(70, 270, 121, 28))
-        self.pushButton.setObjectName("pushButton")
+        self.Dialog.resize(400, 215)
+        self.cancel = QtWidgets.QPushButton(self.Dialog)
+        self.cancel.setGeometry(QtCore.QRect(220, 150, 93, 28))
+        self.cancel.setObjectName("cancel")
+        self.ok = QtWidgets.QPushButton(self.Dialog)
+        self.ok.setGeometry(QtCore.QRect(70, 150, 121, 28))
+        self.ok.setObjectName("ok")
         self.label_5 = QtWidgets.QLabel(self.Dialog)
         self.label_5.setGeometry(QtCore.QRect(70, 70, 101, 20))
         self.label_5.setObjectName("label_5")
         self.label_4 = QtWidgets.QLabel(self.Dialog)
         self.label_4.setGeometry(QtCore.QRect(80, 20, 71, 20))
         self.label_4.setObjectName("label_4")
-        self.lineEdit_3 = QtWidgets.QLineEdit(self.Dialog)
-        self.lineEdit_3.setGeometry(QtCore.QRect(160, 70, 171, 22))
-        self.lineEdit_3.setObjectName("lineEdit_3")
-        self.lineEdit_2 = QtWidgets.QLineEdit(self.Dialog)
-        self.lineEdit_2.setGeometry(QtCore.QRect(160, 20, 171, 22))
-        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.name = QtWidgets.QLineEdit(self.Dialog)
+        self.name.setGeometry(QtCore.QRect(160, 70, 171, 22))
+        self.name.setObjectName("name")
+        self.id = QtWidgets.QLineEdit(self.Dialog)
+        self.id.setGeometry(QtCore.QRect(160, 20, 171, 22))
+        self.id.setObjectName("id")
 
         self.retranslateUi(self.Dialog)
         QtCore.QMetaObject.connectSlotsByName(self.Dialog)
 
-        self.pushButton.clicked.connect(self.add)
-        self.pushButton_2.clicked.connect(self.back)        
+        self.ok.clicked.connect(self.add)
+        self.cancel.clicked.connect(self.back)        
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.pushButton_2.setText(_translate("Dialog", "Cancel"))
-        self.label_6.setText(_translate("Dialog", " Drug Description :"))
-        self.pushButton.setText(_translate("Dialog", "Ok"))
+        self.cancel.setText(_translate("Dialog", "Cancel"))
+        self.ok.setText(_translate("Dialog", "Ok"))
         self.label_5.setText(_translate("Dialog", " Drug Name :"))
         self.label_4.setText(_translate("Dialog", " Drug ID :"))
 
@@ -50,10 +44,10 @@ class Ui_Dialog(object):
     def show(self):
         self.Dialog.show()
 
+
     def add(self):
-        drugDesc = self.textEdit.toPlainText()
-        drugID = self.lineEdit_2.text()
-        drugName = self.lineEdit_3.text()
+        drugID = self.id.text()
+        drugName = self.name.text()
         #TODO add new drug to database
         
 
@@ -65,9 +59,9 @@ class Ui_Dialog(object):
     def back(self):
         self.ui = DrugController.Ui_Dialog()
         self.Dialog.hide()
-        self.ui.show()  
+        self.ui.show() 
+    
 
-        
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
