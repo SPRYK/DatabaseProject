ba0dc38fc370dd392786d331509266b1b394f1c3
Siwat <siwat.keng.pongpanit@gmail.com>
2019-11-25 18:20:15 +0700

    changed search room

--- roomSearch.py
+++ roomSearch.py
@@ -1,55 +1,63 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
 import RoomController
+from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class Ui_Dialog(object):
     def __init__(self):
         self.Dialog = QtWidgets.QDialog()
         self.Dialog.setObjectName("Dialog")
         self.Dialog.resize(400, 300)
-        self.lineEdit = QtWidgets.QLineEdit(self.Dialog)
-        self.lineEdit.setGeometry(QtCore.QRect(180, 30, 161, 22))
-        self.lineEdit.setObjectName("lineEdit")
-        self.pushButton = QtWidgets.QPushButton(self.Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(120, 210, 93, 28))
-        self.pushButton.setObjectName("pushButton")
+        self.id = QtWidgets.QLineEdit(self.Dialog)
+        self.id.setGeometry(QtCore.QRect(180, 30, 161, 22))
+        self.id.setObjectName("id")
+        self.search = QtWidgets.QPushButton(self.Dialog)
+        self.search.setGeometry(QtCore.QRect(120, 210, 93, 28))
+        self.search.setObjectName("search")
         self.textBrowser = QtWidgets.QTextBrowser(self.Dialog)
         self.textBrowser.setGeometry(QtCore.QRect(190, 120, 161, 41))
         self.textBrowser.setObjectName("textBrowser")
         self.label = QtWidgets.QLabel(self.Dialog)
         self.label.setGeometry(QtCore.QRect(40, 30, 161, 20))
         self.label.setObjectName("label")
-        self.pushButton_2 = QtWidgets.QPushButton(self.Dialog)
-        self.pushButton_2.setGeometry(QtCore.QRect(270, 210, 93, 28))
-        self.pushButton_2.setObjectName("pushButton_2")
+        self.cancel = QtWidgets.QPushButton(self.Dialog)
+        self.cancel.setGeometry(QtCore.QRect(270, 210, 93, 28))
+        self.cancel.setObjectName("cancel")
         self.label_2 = QtWidgets.QLabel(self.Dialog)
-        self.label_2.setGeometry(QtCore.QRect(40, 130, 141, 20))
+        self.label_2.setGeometry(QtCore.QRect(70, 130, 111, 20))
         self.label_2.setObjectName("label_2")
+        self.line = QtWidgets.QFrame(self.Dialog)
+        self.line.setGeometry(QtCore.QRect(20, 80, 351, 20))
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
 
         self.retranslateUi(self.Dialog)
         QtCore.QMetaObject.connectSlotsByName(self.Dialog)
 
-        self.pushButton.clicked.connect(self.search)
-        self.pushButton_2.clicked.connect(self.back)        
+
+        self.search.clicked.connect(self.searching)
+        self.cancel.clicked.connect(self.back)         
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.pushButton.setText(_translate("Dialog", "Search"))
+        self.search.setText(_translate("Dialog", "Search"))
         self.textBrowser.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
         self.label.setText(_translate("Dialog", "Search by Room ID :"))
-        self.pushButton_2.setText(_translate("Dialog", "Cancel"))
-        self.label_2.setText(_translate("Dialog", "Selected Department :"))
+        self.cancel.setText(_translate("Dialog", "Cancel"))
+        self.label_2.setText(_translate("Dialog", "Selected Room :"))
 
 
     def show(self):
         self.Dialog.show()
 
-    def search(self):
-        roomID = self.lineEdit.text()
+
+    def searching(self):
+        roomID = self.id.text()
         #TODO add room to self.textBrowser
 
         #example
@@ -62,7 +70,8 @@ class Ui_Dialog(object):
     def back(self):
         self.ui = RoomController.Ui_Dialog()
         self.Dialog.hide()
-        self.ui.show()         
+        self.ui.show()          
+
         
 if __name__ == "__main__":
     import sys
